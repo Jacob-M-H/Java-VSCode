29ba0a54465a6c64fd2c3b2292aa68f6e83b904c
Pvpet <jacobhilst2@gmail.com>
2023-06-22 17:18:14 -0500

    Continued Java notes. After mothers' surgery and eye issues. Thought of a project to make. Basically a version of MS Paint. See Math Engine 1 notes for more.

---
+++ Hello.java
@@ -0,0 +1,14 @@
+//VSCode tutorial suggested file start. 
+//Java langauge server auto loads.
+ 
+
+
+
+/**
+ * Hello
+ */
+public class Hello {
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+    }
+}
