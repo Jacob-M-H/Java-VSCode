e8c76a854cf5fcd9371d3106c1e183d7254eadcb
Pvpet <jacobhilst2@gmail.com>
2023-06-28 02:48:55 -0500

    The bug was caused by pathing. Unclear why.

--- JavaFX Exploration/test1artid/src/main/java/com/testgroupid/MainImplementer.java
+++ JavaFX Exploration/test1artid/src/main/java/com/testgroupid/MainImplementer.java
@@ -166,30 +166,33 @@ public class MainImplementer extends Application{
             //Root node based on fxml file! 
             System.out.println("Try loading test Main fxml!");
             //Parent root=FXMLLoader.load(getClass().getResource("C:/Users/thebl/Java VSCode/JavaFX Exploration/test1artid/src/main/resources/com/testgroupid/testMain.fxml"));
-            try{
-            //System.out.println("Get Path to fxml");
-            //Path path = new File(getClass().getResource("/primary.fxml").getFile()).toPath();
-            //System.out.println("Path found: "+path.toString());
-            String fxml = "testMain";
-            System.out.println("FXMLLoader");
-            FXMLLoader fxmlLoader = new FXMLLoader(MainImplementer.class.getResource(fxml+".fxml"));
-            System.out.println("FXMLLoader.load");
-            Parent root = fxmlLoader.load();
-            //Parent root=FXMLLoader.load(getClass().getResource("C:/Users/thebl/Java VSCode/JavaFX Exploration/test1artid/src/main/resources/com/testgroupid/primary.fxml"));
-            System.out.println("Loaded!");
+        Parent root =null;
+        Parent root2 =null;
+        String fxml = "testMain";
 
+            try{ 
+            System.out.println("Try to disect");
+             root2 = FXMLLoader.load(getClass().getResource(fxml+".fxml"));
+            System.out.println("Okay");
+            
             }
             catch(Exception e){
                 e.printStackTrace();
-                System.out.println("Failed 1!");
+                System.out.println("Failed 1!");  
                 //KEEP: (comment out when above works.)
-                 //Group root = new Group(); //Note there are different root nodes, FUTURE.
+                //Group root = new Group(); //Note there are different root nodes, FUTURE.
             
             }
         
            
+            System.out.println("FXMLLoader");
+            FXMLLoader fxmlLoader = new FXMLLoader(MainImplementer.class.getResource(fxml+".fxml"));
+            System.out.println("FXMLLoader.load");
+            root = fxmlLoader.load();
+            //Parent root=FXMLLoader.load(getClass().getResource("C:/Users/thebl/Java VSCode/JavaFX Exploration/test1artid/src/main/resources/com/testgroupid/primary.fxml"));
+            System.out.println("Loaded!");
             
-            Group root = new Group(); //Note there are different root nodes, FUTURE.
+            //Group root = new Group(); //Note there are different root nodes, FUTURE.
             
             
 
